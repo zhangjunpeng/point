d1f6d4a03b4e5992854811babe6870f6b08fd12c
zhangjunpeng <zhangjunpeng@nextmar.com>
2021-01-04 09:35:19 +0800

    11

--- app/src/main/java/com/zjdx/point/ui/travel/TravelActivity.kt
+++ app/src/main/java/com/zjdx/point/ui/travel/TravelActivity.kt
@@ -175,7 +175,7 @@ class TravelActivity : BaseActivity() {
 
         val myLocationStyle: MyLocationStyle = MyLocationStyle()
         //初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW)
         myLocationStyle.interval(1000) //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
 
         map.setMyLocationStyle(myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW)) //设置定位蓝点的Style
