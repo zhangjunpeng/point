e16927ce58e0a387aca553240a31173097644a1d
zhangjunpeng <zhangjunpeng@nextmar.com>
2021-01-19 11:43:08 +0800

    1.9版本，数据库版本4: 添加bugly

--- app/src/main/java/com/zjdx/point/PointApplication.kt
+++ app/src/main/java/com/zjdx/point/PointApplication.kt
@@ -1,14 +1,20 @@
 package com.zjdx.point
 
 import android.content.Context
+import android.os.Process
+import android.text.TextUtils
 import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
 import com.blankj.utilcode.util.Utils
 import com.tencent.bugly.crashreport.CrashReport
+import com.tencent.bugly.crashreport.CrashReport.UserStrategy
 import com.zjdx.point.data.repository.LocationRepository
 import com.zjdx.point.data.repository.RegisterRepository
 import com.zjdx.point.data.repository.TravelRepository
 import com.zjdx.point.db.MyDataBase
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.IOException
 
 
 class PointApplication : MultiDexApplication() {
@@ -26,13 +32,58 @@ class PointApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        CrashReport.initCrashReport(applicationContext, "42889ffb-d974-4924-972c-98d2e678c64c", true)
+
+
+        initCrash()
 
         Utils.init(this)
+    }
+
+    private fun initCrash() {
+
+// 获取当前包名
+        val packageName = applicationContext.packageName
+// 获取当前进程名
+        val processName = getProcessName(Process.myPid())
+// 设置是否为上报进程
+        val strategy = UserStrategy(applicationContext)
+        strategy.isUploadProcess = processName == null || processName == packageName
+
+        CrashReport.initCrashReport(applicationContext, "90cff47ede", true, strategy)
+
     }
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         MultiDex.install(this)
+    }
+
+    /**
+     * 获取进程号对应的进程名
+     *
+     * @param pid 进程号
+     * @return 进程名
+     */
+    private fun getProcessName(pid: Int): String? {
+        var reader: BufferedReader? = null
+        try {
+            reader = BufferedReader(FileReader("/proc/$pid/cmdline"))
+            var processName: String = reader.readLine()
+            if (!TextUtils.isEmpty(processName)) {
+                processName = processName.trim { it <= ' ' }
+            }
+            return processName
+        } catch (throwable: Throwable) {
+            throwable.printStackTrace()
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close()
+                }
+            } catch (exception: IOException) {
+                exception.printStackTrace()
+            }
+        }
+        return null
     }
 }
