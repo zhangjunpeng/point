e09bd32e7a735268d0a6392d76d04b6ad044dcc4
zjp <zhangjunpeng92@163.com>
2022-08-24 23:02:47 +0800

    1111

--- app/src/main/java/com/zjdx/point/ui/tagging/TagInfoFragment.kt
+++ app/src/main/java/com/zjdx/point/ui/tagging/TagInfoFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.zjdx.point.R
+import com.zjdx.point.databinding.FragmentTagInfoBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,24 +19,20 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TagInfoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentTagInfoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tag_info, container, false)
+        binding=FragmentTagInfoBinding.inflate(inflater)
+        return binding.root
     }
 
     companion object {
@@ -51,10 +48,7 @@ class TagInfoFragment : Fragment() {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             TagInfoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
             }
     }
 }
